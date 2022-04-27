1950449c1f7108c200fcca942a7f8edba8ddfe1b
Lucien Sigayret <lucien.sigayret@protonmail.com>
2022-04-27 08:43:03 +0200

    added ML

--- dashboard.py
+++ dashboard.py
@@ -3,10 +3,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Lasso, LinearRegression, Ridge, ElasticNet
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 st.set_page_config(layout="wide")
 
-st.title("Dashboard")
+st.title("Dashboard - Graphics")
 
 df = pd.read_csv(os.path.join("data", "Capital_Bikeshare_data.csv"), sep=";")
 
@@ -98,3 +105,85 @@ c, _ = st.columns(2)
 
 with c:
     st.write(fig)
+
+df_ml = df.copy()
+
+target_count = df_ml["count"]
+target_registered = df_ml["registered"]
+target_casual = df_ml["casual"]
+df_ml = df_ml.drop(["casual", "registered", "count"], axis=1)
+
+st.title("Machine Learning")
+
+algo = [(LinearRegression(), "Linear Regression"),
+        (Ridge(alpha=0.5), "Ridge Regression"),
+        (Lasso(alpha=0.5), "Lasso Regression"),
+        (ElasticNet(alpha=0.5), "Elastic Net")]
+
+for regression, title in algo:
+    st.subheader(title)
+    co, re, ca = st.columns(3)
+    with co:
+        st.subheader("*Count*")
+        X_train, X_test, y_train, y_test = train_test_split(df_ml,
+                                                            target_count,
+                                                            test_size=0.30,
+                                                            random_state=40)
+        scaler = StandardScaler()
+        std_feats = scaler.fit_transform(X_train)
+        model = regression.fit(std_feats, y_train)
+        pred_test = model.predict(X_test)
+
+        rmse_count = np.sqrt(mean_squared_error(y_test, pred_test))
+        r2_count = r2_score(y_test, pred_test)
+
+    with re:
+        st.subheader("*Registered*")
+        X_train, X_test, y_train, y_test = train_test_split(df_ml,
+                                                            target_registered,
+                                                            test_size=0.30,
+                                                            random_state=40)
+        scaler = StandardScaler()
+        std_feats = scaler.fit_transform(X_train)
+        model = regression.fit(std_feats, y_train)
+        pred_test = model.predict(X_test)
+
+        rmse_registered = np.sqrt(mean_squared_error(y_test, pred_test))
+        r2_registered = r2_score(y_test, pred_test)
+
+    with ca:
+        st.subheader("*Casual*")
+        X_train, X_test, y_train, y_test = train_test_split(df_ml,
+                                                            target_casual,
+                                                            test_size=0.30,
+                                                            random_state=40)
+        scaler = StandardScaler()
+        std_feats = scaler.fit_transform(X_train)
+        model = regression.fit(std_feats, y_train)
+        pred_test = model.predict(X_test)
+
+        rmse_casual = np.sqrt(mean_squared_error(y_test, pred_test))
+        r2_casual = r2_score(y_test, pred_test)
+
+    a, b, c, d, e, f = st.columns(6)
+
+    with a:
+        st.write("RMSE")
+        st.write(np.round(rmse_count, 2))
+    with b:
+        st.write("R2")
+        st.write(np.round(r2_count, 4))
+
+    with c:
+        st.write("RMSE")
+        st.write(np.round(rmse_registered, 2))
+    with d:
+        st.write("R2")
+        st.write(np.round(r2_registered, 4))
+
+    with e:
+        st.write("RMSE")
+        st.write(np.round(rmse_casual, 2))
+    with f:
+        st.write("R2")
+        st.write(np.round(r2_casual, 4))
