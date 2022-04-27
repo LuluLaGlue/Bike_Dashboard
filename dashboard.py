065d9e8c3a69e95cc03816133b4e6620309727bc
Lucien Sigayret <lucien.sigayret@protonmail.com>
2022-04-27 10:14:59 +0200

    snow ptdr

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,22 @@
-import streamlit as st
+import os
+import warnings
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import streamlit as st
 import matplotlib.pyplot as plt
-import os
-import numpy as np
 
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import Lasso, LinearRegression, Ridge, ElasticNet
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso, LinearRegression, Ridge, ElasticNetCV, HuberRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 
+warnings.filterwarnings('ignore')
 st.set_page_config(layout="wide")
 
-st.snow()
+# st.snow()
 
 st.title("Dashboard - Graphics")
 
@@ -38,24 +41,15 @@ cols = ['season', 'month', 'holiday', 'weekday', 'workingday', 'weather']
 for col in cols:
     df[col] = df[col].astype('category')
 
-# fig, ax = plt.subplots(figsize=(20, 5))
-# sns.pointplot(data=df, x='month', y='count', hue='weekday', ax=ax)
-# ax.set(title='Count of bikes during weekdays and weekends')
-# plt.show()
-
-# st.write(df.head(10))
-
 st.subheader("Bikes rented per month and week day")
 
 fig_month, ax_month = plt.subplots(figsize=(20, 5))
 sns.barplot(data=df, x='month', y='count', ax=ax_month)
 ax_month.set(title='Count of bikes during different months')
-# plt.show()
 
 fig_weekday, ax_weekday = plt.subplots(figsize=(20, 5))
 sns.barplot(data=df, x='weekday', y='count', ax=ax_weekday)
 ax_weekday.set(title='Count of bikes during different days')
-# plt.show()
 
 m, w = st.columns(2)
 with m:
@@ -75,14 +69,14 @@ ax_weather.set(title='Count of bikes for different weathers')
 
 s, w = st.columns(2)
 with s:
-    st.write(
-        "Seasons equivalence: 1 - Spring, 2 - Summer, 3 - Fall, 4 - Winter")
     st.write(fig_season)
+    st.caption(
+        "Seasons equivalence: 1 - Spring, 2 - Summer, 3 - Fall, 4 - Winter")
 with w:
-    st.write(
+    st.write(fig_weather)
+    st.caption(
         "Weather equivalence: 1 - Clear Skies, 2 - Misty / Cloudy, 3 - Light Raining / Thunderstorm, 4 - Heavy Raining / Snow"
     )
-    st.write(fig_weather)
 
 st.subheader("Influence of temperature and humidity on users")
 
@@ -108,33 +102,77 @@ c, _ = st.columns(2)
 with c:
     st.write(fig)
 
-df_ml = df.copy()
+df_ml = df
+
+df_ml['count'] = np.log(df_ml['count'])
+df_ml['casual'] = np.log(df_ml['casual'])
+df_ml['registered'] = np.log(df_ml['registered'])
+
+
+def one_hot_encoding(data, column):
+    data = pd.concat(
+        [data,
+         pd.get_dummies(data[column], prefix=column, drop_first=True)],
+        axis=1)
+    data = data.drop([column], axis=1)
+    return data
+
+
+cols = ['season', 'month', 'holiday', 'weekday', 'workingday', 'weather']
+
+for col in cols:
+    df_ml = one_hot_encoding(df_ml, col)
 
 target_count = df_ml["count"]
 target_registered = df_ml["registered"]
 target_casual = df_ml["casual"]
-df_ml = df_ml.drop(["casual", "registered", "count"], axis=1)
+df_ml = df_ml.drop(["atemp", "windspeed", "casual", "registered", "count"],
+                   axis=1)
 
 st.title("Machine Learning")
 
 algo = [(LinearRegression(), "Linear Regression"),
-        (Ridge(alpha=0.5), "Ridge Regression"),
-        (Lasso(alpha=0.5), "Lasso Regression"),
-        (ElasticNet(alpha=0.5), "Elastic Net")]
+        (Ridge(), "Ridge Regression"), (Lasso(), "Lasso Regression"),
+        (ElasticNetCV(), "Elastic Net"),
+        (HuberRegressor(), "Huber Regression"),
+        (RandomForestRegressor(), "Random Forest Regression"),
+        (GradientBoostingRegressor(), "Gradient Boosting Regression"),
+        (ExtraTreesRegressor(), "Extra Trees Regression")]
+available_algo = [
+    "Linear Regression", "Ridge Regression", "Lasso Regression", "Elastic Net",
+    "Huber Regression", "Random Forest Regression",
+    "Gradient Boosting Regression", "Extra Trees Regression"
+]
+to_display = []
 
-for regression, title in algo:
-    st.subheader(title)
+alg_selection = st.multiselect("Select Algorithms", available_algo,
+                               available_algo)
+
+for a in alg_selection:
+    index = available_algo.index(a)
+    to_display.append(algo[index])
+
+for regression, title in to_display:
+    st.header(title)
     co, re, ca = st.columns(3)
     with co:
         st.subheader("*Count*")
         X_train, X_test, y_train, y_test = train_test_split(df_ml,
                                                             target_count,
-                                                            test_size=0.30,
-                                                            random_state=40)
+                                                            test_size=0.25,
+                                                            random_state=42)
         scaler = StandardScaler()
         std_feats = scaler.fit_transform(X_train)
         model = regression.fit(std_feats, y_train)
         pred_test = model.predict(X_test)
+
+        error = y_test - pred_test
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.scatter(y_test, error, color='green')
+        ax.axhline(lw=3, color='black')
+        ax.set_xlabel('Observed')
+        ax.set_ylabel('Error')
+        st.write(fig)
 
         rmse_count = np.sqrt(mean_squared_error(y_test, pred_test))
         r2_count = r2_score(y_test, pred_test)
@@ -143,12 +181,20 @@ for regression, title in algo:
         st.subheader("*Registered*")
         X_train, X_test, y_train, y_test = train_test_split(df_ml,
                                                             target_registered,
-                                                            test_size=0.30,
-                                                            random_state=40)
+                                                            test_size=0.25,
+                                                            random_state=42)
         scaler = StandardScaler()
         std_feats = scaler.fit_transform(X_train)
         model = regression.fit(std_feats, y_train)
         pred_test = model.predict(X_test)
+
+        error = y_test - pred_test
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.scatter(y_test, error, color='blue')
+        ax.axhline(lw=3, color='black')
+        ax.set_xlabel('Observed')
+        ax.set_ylabel('Error')
+        st.write(fig)
 
         rmse_registered = np.sqrt(mean_squared_error(y_test, pred_test))
         r2_registered = r2_score(y_test, pred_test)
@@ -157,35 +203,49 @@ for regression, title in algo:
         st.subheader("*Casual*")
         X_train, X_test, y_train, y_test = train_test_split(df_ml,
                                                             target_casual,
-                                                            test_size=0.30,
-                                                            random_state=40)
+                                                            test_size=0.25,
+                                                            random_state=42)
         scaler = StandardScaler()
         std_feats = scaler.fit_transform(X_train)
         model = regression.fit(std_feats, y_train)
         pred_test = model.predict(X_test)
 
+        error = y_test - pred_test
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.scatter(y_test, error, color='purple')
+        ax.axhline(lw=3, color='black')
+        ax.set_xlabel('Observed')
+        ax.set_ylabel('Error')
+        st.write(fig)
+
         rmse_casual = np.sqrt(mean_squared_error(y_test, pred_test))
         r2_casual = r2_score(y_test, pred_test)
 
-    a, b, c, d, e, f = st.columns(6)
+    z, a, b, y, c, d, x, e, f = st.columns(9)
 
+    with z:
+        st.write()
     with a:
         st.write("RMSE")
-        st.write(np.round(rmse_count, 2))
+        st.write(np.round(rmse_count, 4))
     with b:
         st.write("R2")
         st.write(np.round(r2_count, 4))
 
+    with y:
+        st.write()
     with c:
         st.write("RMSE")
-        st.write(np.round(rmse_registered, 2))
+        st.write(np.round(rmse_registered, 4))
     with d:
         st.write("R2")
         st.write(np.round(r2_registered, 4))
 
+    with x:
+        st.write()
     with e:
         st.write("RMSE")
-        st.write(np.round(rmse_casual, 2))
+        st.write(np.round(rmse_casual, 4))
     with f:
         st.write("R2")
         st.write(np.round(r2_casual, 4))
